2b5f1433c0c1422194099769f6ba6c671b190772
habib mbow <hhmbow@hotmail.com>
2024-04-16 15:49:14 +0200

    commit

--- ExerciseLexiconGit/Program.cs
+++ ExerciseLexiconGit/Program.cs
@@ -15,7 +15,7 @@ namespace ExerciseLexiconGit
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days... You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days.. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             Console.WriteLine("Felix xhär");
